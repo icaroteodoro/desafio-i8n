4a5929f97bf999c55681f616e962ae4de787a39b
Ícaro Teodoro <icaro.teodoro84@icloud.com>
2025-06-15 23:01:51 -0300

    feat: add order and order_item modules in app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,10 +7,12 @@ import { UserService } from './users/users.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { JwtModule } from '@nestjs/jwt';
+import { OrderModule } from './order/order.module';
+import { OrderItemsModule } from './order-items/order-items.module';
 
 
 @Module({
-  imports: [AuthModule, UsersModule, JwtModule],
+  imports: [AuthModule, UsersModule, JwtModule, OrderModule, OrderItemsModule],
   controllers: [AppController, AuthController],
   providers: [AppService, AuthService, UserService],
 })
