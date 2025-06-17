4bf2fa33683ab3ae1a9c70ae41c9a77c43ea941d
Ícaro Teodoro <icaro.teodoro84@icloud.com>
2025-06-17 18:47:24 -0300

    feat: add guardian to order and products endpoints

--- backend/src/order/order.controller.ts
+++ backend/src/order/order.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('order')
 export class OrderController {
@@ -17,22 +18,26 @@ export class OrderController {
     return this.orderService.findAll();
   }
 
-  @Get(':id')
+  @Get(':id') 
+  @UseGuards(AuthGuard)
   findOne(@Param('id') id: string) {
     return this.orderService.findOne(id);
   }
 
   @Get('user/:userId')
+  @UseGuards(AuthGuard)
   findAllByUserId(@Param('userId') userId: string) {
     return this.orderService.findAllOrdersByUserId(userId);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard)
   update(@Param('id') id: string, @Body() updateOrderDto: UpdateOrderDto) {
     return this.orderService.update(id, updateOrderDto);
   }
 
   @Delete(':id')
+  @UseGuards(AuthGuard)
   remove(@Param('id') id: string) {
     return this.orderService.remove(id);
   }
--- backend/src/product/product.controller.ts
+++ backend/src/product/product.controller.ts
@@ -6,28 +6,34 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { ProductService } from './product.service';
+import { AuthGuard } from 'src/auth/auth.guard';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
   @Get("brazilian")
+  @UseGuards(AuthGuard)
   findAllBrazilian() {
     return this.productService.findAllBrazilan();
   }
 
   @Get('brazilian/:id')
+  @UseGuards(AuthGuard)
   findOneBrazilian(@Param('id') id: number) {
     return this.productService.findOneBrazilian(id);
   }
   @Get("european")
+  @UseGuards(AuthGuard)
   findAllEuropean() {
     return this.productService.findAllEuropean();
   }
 
   @Get('european/:id')
+  @UseGuards(AuthGuard)
   findOneEuropean(@Param('id') id: number) {
     return this.productService.findOneEuropean(id);
   }
