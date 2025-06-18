ee764a32f9b66dc679eaa67116ff66e0dae7a5b6
Ícaro Teodoro <icaro.teodoro84@icloud.com>
2025-06-18 01:24:09 -0300

    feat: add interceptor to insert token in the header of all requests

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -46,6 +46,37 @@ api.interceptors.request.use(
   (error) => Promise.reject(error)
 );
 
+api.interceptors.request.use(
+  async (config) => {
+    const token = getToken();
+    const refreshToken = getRefreshToken();
+    if(!token) return config; 
+
+    if (isTokenExpired(token)) {
+      if(isTokenExpired(refreshToken)){
+        logout()
+      }else{
+        const response = await axios.post(
+          `${BASE_URL}/auth/refresh-token`,
+          { refreshToken }
+        );
+  
+        const { token: newToken, refreshToken: newRefreshToken } = response.data;
+        saveToken(newToken);
+        saveRefreshToken(newRefreshToken);
+  
+        config.headers.Authorization = `Bearer ${newToken}`;
+        return config;
+      }
+    }
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 
 export default api;
