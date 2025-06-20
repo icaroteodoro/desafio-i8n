f7460b069fb051e7b638b742c8093f3dd4d0d489
Ícaro Teodoro <icaro.teodoro84@icloud.com>
2025-06-19 23:53:00 -0300

    fix: change app background color

--- frontend/src/app/(private)/layout.tsx
+++ frontend/src/app/(private)/layout.tsx
@@ -6,7 +6,7 @@ import { CartProvider } from "@/context/cartContext";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <CartProvider>
-      <main className="w-full h-screen">
+      <main className="w-full min-h-screen max-h-max bg-zinc-100">
         <Header />
         <Toaster />
         {children}
